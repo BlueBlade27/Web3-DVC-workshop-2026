665ef7f2eb6bc5fa1fed7559131d7dc053ec6d29
Rose Gu <yztjs.rose@gmail.com>
2026-01-19 20:11:35 -0500

    add interactions

--- src/components/content/MovieCard.jsx
+++ src/components/content/MovieCard.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {userState} from 'react';
 import './MovieCard.css';
 
 const MovieCard = ({ movie }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   const handleClick = () => {
     console.log('Movie clicked:', movie.id);
   };
 
   return (
-    <div className="movie-card" data-id={movie.id} onClick={handleClick}>
+    <div className="movie-card" data-id={movie.id} onClick={handleClick} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
       <img src={movie.image} alt={movie.title} />
       <div className="movie-card-info">
         <div className="movie-card-title">{movie.title}</div>
